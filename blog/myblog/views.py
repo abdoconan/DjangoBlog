2a0f4750e385b2f7f72489300da2cc13f674f07a
abdoconan <abdo1258conan@gmail.com>
2020-12-21 23:29:53 +0200

    added database with django

--- blog/myblog/views.py
+++ blog/myblog/views.py
@@ -1,28 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Posts
 
 #Posts
-posts = [
-    {
-        'author':'Youstina Nagy',
-        'title':'First post',
-        'contant':'this is my first post',
-        'date_posted': 'Augest 1st, 2019'
-    },
-    {
-        'author':'Abdo Conan',
-        'title':'Second post',
-        'contant':'this is my second post',
-        'date_posted': 'September 23th, 2019'
-    },
+# posts = [
+#     {
+#         'author':'Youstina Nagy',
+#         'title':'First post',
+#         'contant':'this is my first post',
+#         'date_posted': 'Augest 1st, 2019'
+#     },
+#     {
+#         'author':'Abdo Conan',
+#         'title':'Second post',
+#         'contant':'this is my second post',
+#         'date_posted': 'September 23th, 2019'
+#     },
 
-]
+# ]
 
 
 # Create your views here.
 def home(request):
     context = {
-        'posts': posts,
+        'posts': Posts.objects.all(),
     }
     return render(request, 'myblog/home.html', context)
 
