1c62a4d2458e81b298b2c5dedd64c54f5b13ee7b
abdoconan <abdo1258conan@gmail.com>
2020-12-23 11:23:13 +0200

    added login and logout and profile templates

--- blog/Users/views.py
+++ blog/Users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -10,11 +11,15 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, 'Account created for {}!'.format(username))
-            return redirect('blog-home')
+            messages.success(request, 'Your account has been created! you are now able to LogIn')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, "Users/register.html", {'form':form})
+
+@login_required
+def profile(request):
+    return render(request, 'Users/profile.html')
 
 
 
--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -15,10 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from Users.views import register as user_register
+from Users.views import profile as user_profile
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/',user_register, name = "register"),
+    path('profile/',user_profile, name = "profile"),
+    path('login/',auth_views.LoginView.as_view(template_name = 'Users/login.html'), name = "login"),
+    path('logout/',auth_views.LogoutView.as_view(template_name = 'Users/logout.html'), name = "logout"),
     path('', include('myblog.urls')),
 ]
